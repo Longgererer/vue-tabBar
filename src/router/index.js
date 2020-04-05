bb208f38d3465a23da696362f0f34cd34d7daa99
xiaolong Hao <2072451919@qq.com>
2020-04-05 08:49:37 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import tabBar from '../components/tabBar'
 Vue.use(Router)
 
 export default new Router({
-  // routes: [
-  //   {
-  //     path: '/',
-  //     name: 'tabBar',
-  //     component: tabBar
-  //   }
-  // ]
+  routes: [
+    {
+      path: '/',
+      name: 'helloWorld',
+      component: tabBar,
+      // children: [
+      //   {
+      //     path: 'user',
+      //     name: 'user',
+      //
+      //   }
+      //
+      // ]
+    },
+
+  ]
 })
